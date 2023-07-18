4a5cf332b9e28967ce2e27b491c2c8c16b47a748
David Jensen <david@dkjensen.com>
2023-07-18 10:26:27 -0400

    feat: update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,22 +1,16 @@
 <svelte:head>
     <title>Chicken Nanny</title>
-    <script>
-        (g=>{var h,a,k,p="The Google Maps JavaScript API",c="google",l="importLibrary",q="__ib__",m=document,b=window;b=b[c]||(b[c]={});var d=b.maps||(b.maps={}),r=new Set,e=new URLSearchParams,u=()=>h||(h=new Promise(async(f,n)=>{await (a=m.createElement("script"));e.set("libraries",[...r]+"");for(k in g)e.set(k.replace(/[A-Z]/g,t=>"_"+t[0].toLowerCase()),g[k]);e.set("callback",c+".maps."+q);a.src=`https://maps.${c}apis.com/maps/api/js?`+e;d[q]=f;a.onerror=()=>h=n(Error(p+" could not load."));a.nonce=m.querySelector("script[nonce]")?.nonce||"";m.head.append(a)}));d[l]?console.warn(p+" only loads once. Ignoring:",g):d[l]=(f,...n)=>r.add(f)&&u().then(()=>d[l](f,...n))})({
-          key: "AIzaSyAO5_X5yWdTiHWvXXMVJlcpk7trCcTqRFY",
-          v: "weekly",
-          // Use the 'v' parameter to indicate the version to use (weekly, beta, alpha, etc.).
-          // Add other bootstrap parameters as needed, using camel case.
-        });
-      </script>
 </svelte:head>
 
 <script>
     import { signIn, signOut } from "@auth/sveltekit/client"
     import { page } from "$app/stores"
-    import { Loader } from '@googlemaps/js-api-loader';
+    import * as GMaps from '@googlemaps/js-api-loader'
+    
+    const { Loader } = GMaps;
 
     const loader = new Loader({
-    apiKey: "AIzaSyAO5_X5yWdTiHWvXXMVJlcpk7trCcTqRFY",
+        apiKey: "AIzaSyAO5_X5yWdTiHWvXXMVJlcpk7trCcTqRFY",
     });
 
     const mapOptions = {
@@ -40,8 +34,6 @@ loader
   });
 </script>
 
-<div id="map"></div>
-
 <p>
     {#if $page.data.session}
       {#if $page.data.session.user?.image}
@@ -61,9 +53,11 @@ loader
     {/if}
   </p>
 
+<div id="map"></div>
+
 <style>
     #map {
-        width: 100vw;
+        width: 100%;
         height: 100vh;
     }
 </style>
